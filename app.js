c1456787f65afab2373b175f90a0c518123179e2
DeAndre' White <111456327+DWhite3112@users.noreply.github.com>
2022-11-02 13:14:22 -0500

    checking end points

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/kings', kingsRouter)
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/kings', kingsRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
